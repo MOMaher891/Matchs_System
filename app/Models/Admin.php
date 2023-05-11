676931a5850376869eb3f779a02f39d6cbc932c6
Eng.Mohamed Maher <75702799+MOMaher891@users.noreply.github.com>
2023-05-11 15:34:57 +0300

    Create RelationShips

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -7,7 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Admin extends Model
 {
+    protected $fillable = ['name','phone','email','password','address','image','is_blocked'];
+    protected $timestamps = false;
     use HasFactory;
 
+    /**
+     * Relations
+    */
+
+    public function stadium(){
+        return $this->hasMany(Stadium::class);
+    }
+
+    public function booking(){
+        return $this->hasManyThrough(Booking::class,Stadium::class);
+    }
+    public function booking_request(){
+        return $this->hasManyThrough(BookRequest::class,Stadium::class);
+    }
 
 }
--- app/Models/Time.php
+++ app/Models/Time.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Time extends Model
 {
     use HasFactory;
+
+    /**
+     * Relations
+    */
+
+    public function book_time(){
+        return $this->belongsToMany(Booking::class,'book_times','book_id','time_id');
+    }
 }
