60609cb5fa119f36ef9a77f34b635f63bb5bf252
mohamedmagdy20 <86977873+mohamedmagdy20@users.noreply.github.com>
2023-05-14 19:20:47 +0300

    Solve Bug

--- resources/views/superadmin/layouts/sidebar.blade.php
+++ resources/views/superadmin/layouts/sidebar.blade.php
@@ -8,23 +8,23 @@
             </a>
         </li>
         @if (auth()->user()->is_admin == true)
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#supder_admin" aria-expanded="false"
-                aria-controls="supder_admin">
-                <i class="icon-layout menu-icon"></i>
-                <span class="menu-title">Admins</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="supder_admin">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('super_admin.admins.index') }}">Show
-                            Admins</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('super_admin.admins.create') }}">Add
-                            Admin</a>
-                    </li>
-                </ul>
-            </div>
-        </li>
+           <li class="nav-item">
+               <a class="nav-link" data-toggle="collapse" href="#supder_admin" aria-expanded="false"
+                   aria-controls="supder_admin">
+                   <i class="icon-layout menu-icon"></i>
+                   <span class="menu-title">Admins</span>
+                   <i class="menu-arrow"></i>
+               </a>
+               <div class="collapse" id="supder_admin">
+                   <ul class="nav flex-column sub-menu">
+                       <li class="nav-item"> <a class="nav-link" href="{{ route('super_admin.admins.index') }}">Show
+                               Admins</a></li>
+                       <li class="nav-item"><a class="nav-link" href="{{ route('super_admin.admins.create') }}">Add
+                               Admin</a>
+                       </li>
+                   </ul>
+               </div>
+           </li>
         @endif
         <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#users" aria-expanded="false" aria-controls="users">
@@ -43,7 +43,6 @@
                 </ul>
             </div>
         </li>
-
         <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#stadium" aria-expanded="false" aria-controls="stadium">
                 <i class="icon-layout menu-icon"></i>
@@ -72,22 +71,14 @@
                             Clients</a></li>
                     {{-- <li class="nav-item"><a class="nav-link" href="{{ route('super_admin.clients.create') }}">Add
                             Client</a> --}}
-        </li>
-    </ul>
-    </div>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false"
-            aria-controls="form-elements">
-            <i class="icon-columns menu-icon"></i>
-            <span class="menu-title">Form elements</span>
-            <i class="menu-arrow"></i>      
-      </li>
-    
-    
-
-
+        </li>    
+        {{-- <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false"
+                aria-controls="form-elements">
+                <i class="icon-columns menu-icon"></i>
+                <span class="menu-title">Form elements</span>
+                <i class="menu-arrow"></i>      
+        </li> --}}
     </ul>
 </nav>
 <!-- partial -->
