faabab79ecf12a415700347e22fdb8f6cb8cb5bb
mohamedmagdy20 <86977873+mohamedmagdy20@users.noreply.github.com>
2023-05-18 21:02:56 +0300

    Registeration and Verifiy Phone with WhatsApp

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Client\Auth\AuthController;
 use App\Http\Controllers\WebSite\WebsiteController;
 use Illuminate\Support\Facades\Route;
-
+use App\Utils\WhatsApp;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,12 +18,25 @@ use Illuminate\Support\Facades\Route;
 Route::group(['controller'=>WebsiteController::class],function(){
     Route::get('/','index');
 });
+Route::get('start-test-message',function(){
+    // return 'test message';
+    $message  =new WhatsApp("+201113050566",'magdy',rand(10000,99999));
+    $message->startConversation();
+    return $message->sendingWhatsAppMessage();
+
+    // return $message->sendingWhatsAppMessage();
+});
+// Route::get('test-message',function(){
+//     // return 'test message';
+//     $message  =new WhatsApp("+201113050566",'magdy');
+// });
+
 
 Route::get('register',[AuthController::class,'registerView'])->name('register.view');
 Route::get('verifiy',[AuthController::class,'verifiyView'])->name('register.verifiy-view');
 Route::post('register',[AuthController::class,'register'])->name('register');
-Route::post('verifiy',[AuthController::class,''])->name('verifiy');
-
+Route::post('verifiy',[AuthController::class,'verifiy'])->name('verifiy');
+Route::get('resend',[AuthController::class,'resend'])->name('resend');
 Route::post('login',[AuthController::class,'login'])->name('client.login');
 
 Route::get('client/logout',[AuthController::class,'logout'])->name('client.logout')->middleware('auth:client');
