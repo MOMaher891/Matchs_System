23023c2bc244bc2b0a1beb038c9c0c66f2b22a12
mohamedmagdy20 <86977873+mohamedmagdy20@users.noreply.github.com>
2023-06-16 01:19:00 +0300

    Search Stadiums

--- app/Http/Controllers/WebSite/StadiumsController.php
+++ app/Http/Controllers/WebSite/StadiumsController.php
@@ -3,15 +3,34 @@
 namespace App\Http\Controllers\Website;
 
 use App\Http\Controllers\Controller;
+use App\Models\Booking;
+use App\Models\BookTime;
 use App\Models\Stadium;
+use App\Models\Time;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class StadiumsController extends Controller
 {
     //
-    public function index()
+    public function index(Request $request)
     {
-        $data = Stadium::paginate(9);
-        return view('website.stadiums', ['data'=>$data]);
+        $data = Stadium::latest();
+        if($request->get('date') && $request->get('time_from') && $request->get('time_to'))
+        {
+            $bookedTime = BookTime::whereIn('time_id',[$request->get('time_from'),$request->get('time_to')])
+            ->where('date',$request->get('date'))->pluck('book_id');
+            $bookedTime =  json_decode(json_encode($bookedTime), true);;
+         
+            $bookings = Booking::whereIn('id',$bookedTime)->pluck('stadium_id');
+            $data->whereNotIn('id',$bookings);
+        }
+
+        if( $request->get('region'))
+        {
+            $data->where('region_id',$request->region);
+        }
+       
+        return view('website.stadiums', ['data'=>$data->paginate(9)]);
     }
 }
--- resources/views/website/layouts/layout.blade.php
+++ resources/views/website/layouts/layout.blade.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') </title>
 
-    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
-
+    {{-- <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" /> --}}
+    <link rel="stylesheet" href="{{asset('Admin/cdns/swiper.css')}}">
     {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" /> --}}
     <link rel="stylesheet" href="{{asset('Admin/cdns/bootstrap.min.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
@@ -22,7 +22,7 @@
     <link rel="stylesheet" href="{{ asset('Admin/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
     <link rel="stylesheet" href="{{ asset('Admin/vendors/datatables/datatables.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('Admin/js/select.dataTables.min.css') }}">
-
+    <link rel="stylesheet" href="{{asset('website/form.css')}}">
     <style>
         html,
         body {
@@ -90,8 +90,8 @@
     <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js" defer data-deferred="1"></script>
     <script src="{{ asset('website/main.js') }}" defer data-deferred="1"></script>
     <!-- Swiper JS -->
-    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
-
+    {{-- <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script> --}}
+    <script src="{{asset('Admin/cdns/swiper.js')}}"></script>
     <!-- Initialize Swiper -->
     <script>
         var swiper = new Swiper(".mySwiper", {
@@ -115,7 +115,7 @@
     <script src="http://code.jquery.com/jquery-3.5.1.js"></script>
 
 
-    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js" defer data-deferred="1"></script>
+    {{-- <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js" defer data-deferred="1"></script> --}}
     <script src="{{ asset('website/main.js') }}" defer data-deferred="1"></script>
     <script src="{{ asset('website/calendar.js') }}" defer data-deferred="1"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.0/js/toastr.js"></script>
